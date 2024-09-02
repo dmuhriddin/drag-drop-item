e3b23e3788199c814e01b3c80bcceddc208dd648
davlatovmuhriddin1 <muhriddind819@gmail.com>
2024-09-02 06:25:15 +0300

    drag drop item [html, css, javascript]

---
+++ script.js
@@ -0,0 +1,27 @@
+let lists = document.querySelectorAll(".list");
+let rightBox = document.querySelector("#right");
+let leftBox = document.querySelector("#left");
+
+lists.forEach((list) => {
+  list.addEventListener("dragstart", (e) => {
+    let selected = e.target;
+
+    rightBox.addEventListener("dragover", (e) => {
+      e.preventDefault();
+    });
+
+    rightBox.addEventListener("drop", (e) => {
+      rightBox.appendChild(selected);
+      selected = null;
+    });
+
+    leftBox.addEventListener("dragover", (e) => {
+      e.preventDefault();
+    });
+
+    leftBox.addEventListener("drop", (e) => {
+      leftBox.appendChild(selected);
+      selected = null;
+    });
+  });
+});
